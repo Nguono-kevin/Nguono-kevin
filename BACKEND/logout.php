5bb497332ddadf79cf3372f977647206f01b61e3
kevin nguono odhiambo <kevinnguono19@gmail.com>
2025-10-20 19:47:29 +0300

    commit

--- BACKEND/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-// logout.php
-header('Content-Type: application/json');
-session_start();
-session_unset();
-session_destroy();
-echo json_encode(["status" => "success", "message" => "Logged out successfully"]);
-?>
